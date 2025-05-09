2c80af45b507a03137b0f882d9866f39741206e9
PaccyC <abayisengaaimepacifique@gmail.com>
2025-05-09 16:15:48 +0200

    init

--- array_syntax_exercises/MaxNum.cpp
+++ array_syntax_exercises/MaxNum.cpp
@@ -1,24 +1,3 @@
-// #include<iostream>
-// using namespace std;
-
-// int main(){
-//     int array[]{14,3,4,6,7,41,56,11,25,9},max,min;
-//     max=array[0];
-//     min=array[0];
-//     for(int i=0;i<12;i++){
-//         if(array[i]>max){
-//             max=array[i];
-//         }
-//         if(array[i]<min){
-//             min=array[i];
-//         }
-//     }
-//     cout<<"The maximum number: "<<max<<endl;
-//     cout<<"The minimum number: "<<min<<endl;
-// }
-
-
-// write a program to find the min and max number in an array
 
 #include<iostream>
 using namespace std;
